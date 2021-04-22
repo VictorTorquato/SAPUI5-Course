d94ff905c9e57b345b4eb1b9ff245bbcbbe92539
Victor Torquato <victortorquatob@gmail.com>
2021-04-22 10:23:24 -0300

    Added view and controller

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,13 @@
 sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
 
-], function () {
+], function (XMLView) {
     "use strict";
 
-    alert("UI5 is ready to go!");
+    XMLView.create({
+        viewName:"sap.ui.demo.walkthrough.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
+
 });
