33b92c9f58577042082acf551d76462bd0b81730
Victor Torquato <victortorquatob@gmail.com>
2021-04-23 11:14:48 -0300

    Added Dialog button

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,11 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
 
-], function (Controller, MessageToast) {
+], function (Controller, MessageToast, Fragment) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+		
 		onShowHello: function () {
 			// Read msg from i18n model
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
@@ -13,6 +15,25 @@ sap.ui.define([
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			// Show message
 			MessageToast.show(sMsg);
+		},
+		onOpenDialog: function () {
+			var oView = this.getView();
+
+			// Create the dialog lazily
+			if(!this.byId("helloDialog"))
+			{
+				// Load async XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "sap.ui.demo.walkthrough.view.HelloDialog"
+				}).then(function (oDialog) {
+					// Connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			};
 		}
 
 	});
